d334a7d9c71df308542d2f8c92e40c552ca5cdab
icehong <icehong@gmail.com>
2017-07-23 01:36:38 +0800

    first version

---
+++ autoload.js
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var para_cart;
+document.getElementById('enterprise-nav-4').click();
+
+chrome.storage.sync.get('foucs_id',function (obj) {
+  window.find(obj["foucs_id"],false,true);
+  
+  var all = document.getElementById("enterprise-content-9-国内内部处理机");
+  var childs = all.childNodes; 
+	for(i=0; i < childs.length;i++)
+	{
+		//console.log(childs[i]);		
+		if(childs[i].nodeName != "#text"){
+			if(childs[i].innerText.indexOf(obj["foucs_id"]) >= 0
+				&& childs[i].innerHTML.indexOf("加入购物车") >= 0 ){
+					//childs[i].childNodes[9].childNodes[1].click();
+				para_cart = childs[i].childNodes[9] ;
+				
+				var MutationObserver = window.WebKitMutationObserver;
+				var observer = new MutationObserver(function(mutations) {
+					mutations.forEach(function(mutation) {
+					console.log('old', mutation.oldValue);
+					console.log('new', mutation.target.style.cssText);
+					setTimeout("para_cart.childNodes[1].click();",1)
+					});    
+				});
+
+				var config = { attributes: true, attributeOldValue: true };
+				observer.observe(para_cart, config);
+
+			}						
+		}		
+	};
+});
---
+++ popup.js
@@ -0,0 +1,17 @@
+﻿function store_foucs_id(){
+	chrome.storage.sync.set({'foucs_id':document.getElementById('foucs_id').value},function(){
+		document.getElementById('status').textContent = '设置完成';
+	});
+}
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', store_foucs_id);
+
+  chrome.storage.sync.get('foucs_id',function (obj) {
+	  document.getElementById('foucs_id').value = obj["foucs_id"];
+	  console.log(obj["foucs_id"]);
+  });
+});
